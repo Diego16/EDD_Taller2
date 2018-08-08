3cc42990661415f936d26139d741f3893ca8fc9d
Diego Guevara <diegogb0113@gmail.com>
2018-08-08 14:45:20 -0500

    Fix

--- NextAnagram.hxx
+++ NextAnagram.hxx
@@ -26,27 +26,10 @@ TList NextAnagram( const TList& lst )
   for(lIt=lst.begin();lIt!=lst.end();lIt++)
     s.push(*lIt);
 
-  std::stack< TValue > s2;
-  std::queue< TValue > q2;
-  TValue aux;
-  std::cout<<"Inital stack: "<<std::endl;
-  while(!s.empty())
-  {
-    aux=s.top();
-    std::cout<<s.top();
-    s.pop();
-    s2.push(aux);
-  }
-  while(!s2.empty())
-  {
-    s.push(s2.top());
-    s2.pop();
-  }
   // 2. Try to find a pivot
   finished = false;
   v_aux = s.top( );
   s.pop( );
-  std::cout<<std::endl<<"1 v_aux: "<<v_aux;
   do
   {
     q.push( v_aux );
@@ -63,39 +46,11 @@ TList NextAnagram( const TList& lst )
       v_aux=s.top();
       s.pop();
     } // fi
-    std::cout<<std::endl<<"2 v_aux: "<<v_aux<<" last_v: "<<last_v;
   } while( v_aux > last_v );
-  std::cout<<std::endl<<"Queue:"<<std::endl;
-  while(!q.empty())
-  {
-    aux=q.front();
-    std::cout<<q.front();
-    q.pop();
-    q2.push(aux);
-  }
-  while(!q2.empty())
-  {
-    q.push(q2.front());
-    q2.pop();
-  }
-  std::cout<<"Stack: "<<std::endl;
-  while(!s.empty())
-  {
-    aux=s.top();
-    std::cout<<s.top();
-    s.pop();
-    s2.push(aux);
-  }
-  while(!s2.empty())
-  {
-    s.push(s2.top());
-    s2.pop();
-  }
   // 2.5. if pivot has been found...
   if( !finished )
   {
     pivot = v_aux;
-    std::cout<<std::endl<<"Front queue: "<<q.front()<<" Pivot: "<<pivot<<std::endl;
     // 3. Find a value just below pivot
     /** TODO #3 **/
     do
@@ -104,36 +59,9 @@ TList NextAnagram( const TList& lst )
       q.pop();
     } while( pivot > q.back());
     // 4. Put it into stack
-    std::cout<<"v_aux below: "<<v_aux<<std::endl;
     s.push( v_aux );
-    std::cout<<"Stack below pivot: "<<std::endl;
-    while(!s.empty())
-    {
-      aux=s.top();
-      std::cout<<s.top();
-      s.pop();
-      s2.push(aux);
-    }
-    while(!s2.empty())
-    {
-      s.push(s2.top());
-      s2.pop();
-    }
     // 5. Put pivot back to queue
     q.push( pivot );
-    std::cout<<std::endl<<"Queue below pivot:"<<std::endl;
-    while(!q.empty())
-    {
-      aux=q.front();
-      std::cout<<q.front();
-      q.pop();
-      q2.push(aux);
-    }
-    while(!q2.empty())
-    {
-      q.push(q2.front());
-      q2.pop();
-    }
     // 6. Find the value just above pivot
     /** TODO #4 **/
     do
@@ -142,55 +70,14 @@ TList NextAnagram( const TList& lst )
       q.pop();
     } while( pivot < v_aux);
     // 7. Put it into stack
-    std::cout<<std::endl<<"v_aux above: "<<v_aux<<std::endl;
     s.push( v_aux );
-    std::cout<<"Stack above pivot: "<<std::endl;
-    while(!s.empty())
-    {
-      aux=s.top();
-      std::cout<<s.top();
-      s.pop();
-      s2.push(aux);
-    }
-    while(!s2.empty())
-    {
-      s.push(s2.top());
-      s2.pop();
-    }
-
   } // fi
-  std::cout<<std::endl<<"Final queue:"<<std::endl;
-  while(!q.empty())
-  {
-    aux=q.front();
-    std::cout<<q.front();
-    q.pop();
-    q2.push(aux);
-  }
-  while(!q2.empty())
-  {
-    q.push(q2.front());
-    q2.pop();
-  }
   // 8. Finish filling the stack by emptying the queue
   /** TODO #5 **/
   while(!q.empty())
   {
     s.push(q.front());
     q.pop();
-  }
-  std::cout<<std::endl<<"Final stack"<<std::endl;
-  while(!s.empty())
-  {
-    aux=s.top();
-    std::cout<<s.top();
-    s.pop();
-    s2.push(aux);
-  }
-  while(!s2.empty())
-  {
-    s.push(s2.top());
-    s2.pop();
   }
   // 9. Fill the final answer in reverse order by emptying the stack
   /** TODO #6 **/
@@ -200,10 +87,6 @@ TList NextAnagram( const TList& lst )
     s.pop();
   }
   // 10. Return
-  std::cout<<std::endl<<"res: ";
-  for(lIt=lst.begin();lIt!=lst.end();lIt++)
-    std::cout<<*lIt;
-  std::cout<<std::endl;
   return( res );
 }
 
